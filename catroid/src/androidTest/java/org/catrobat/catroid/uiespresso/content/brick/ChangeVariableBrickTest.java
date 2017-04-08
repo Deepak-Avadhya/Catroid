58464c938acae2e09bd5dc71760166c28bd1e3e5
Deepak <deepakavadhyaa@gmail.com>
2017-04-09 04:02:47 +0530

    Espresso Ui brickChangeVariableTest

--- catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/ChangeVariableBrickTest.java
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/content/brick/ChangeVariableBrickTest.java
@@ -4,7 +4,6 @@ import android.support.test.espresso.Espresso;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.catrobat.catroid.R;
-import org.catrobat.catroid.content.Project;
 import org.catrobat.catroid.content.Script;
 import org.catrobat.catroid.content.bricks.ChangeVariableBrick;
 import org.catrobat.catroid.ui.ScriptActivity;
@@ -16,22 +15,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.longClick;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.isDialog;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.catrobat.catroid.R.id.brick_change_variable_label;
 import static org.catrobat.catroid.uiespresso.content.brick.BrickTestUtils.checkIfBrickAtPositionShowsString;
-import static org.catrobat.catroid.uiespresso.content.brick.BrickTestUtils.checkIfSpinnerOnBrickAtPositionShowsString;
-import static org.catrobat.catroid.uiespresso.content.brick.BrickTestUtils.clickAndSelectFromSpinnerOnBrickAtPosition;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.instanceOf;
+import static org.catrobat.catroid.uiespresso.content.brick.BrickTestUtils.onScriptList;
 
 @RunWith(AndroidJUnit4.class)
 public class ChangeVariableBrickTest {
@@ -52,6 +47,7 @@ public class ChangeVariableBrickTest {
 
         checkIfBrickAtPositionShowsString(0, R.string.brick_when_started);
         checkIfBrickAtPositionShowsString(1, R.string.brick_change_variable);
+
         String userVariableName = "testVariable1";
         String secondUserVariableName = "testVariable2";
 
@@ -60,41 +56,34 @@ public class ChangeVariableBrickTest {
         onView(withId(R.id.dialog_formula_editor_data_name_edit_text)).perform(typeText(userVariableName));
         onView(withId(android.R.id.button1)).perform(click());
         onView(withId(brick_change_variable_label)).check(matches(isDisplayed()));
-
-        checkIfSpinnerOnBrickAtPositionShowsString(R.id.change_variable_spinner, 1, R.string
-                .testName1);
-        clickAndSelectFromSpinnerOnBrickAtPosition(R.id.change_variable_spinner, 1, R.string
-                .brick_variable_spinner_create_new_variable);
-        //onData(instanceOf(ChangeVariableBrick.class)).inAdapterView(withId(R.id.change_variable_spinner)).onChildView(withText////(userVariableName)).check(matches(isDisplayed()));
-
+        onScriptList().atPosition(1).onChildView(withId(R.id.change_variable_spinner)).onChildView(withText(userVariableName))
+                .check(matches(isDisplayed()));
+        onScriptList().atPosition(1).onChildView(withId(R.id.change_variable_spinner))
+                .perform(click());
+        onView(withText(R.string.brick_variable_spinner_create_new_variable))
+                .perform(click());
         onView(withText(R.string.formula_editor_variable_dialog_title)).check(matches(isDisplayed()));
-
         onView(withId(R.id.dialog_formula_editor_data_name_edit_text))
                 .perform(typeText(secondUserVariableName));
         onView(withId(android.R.id.button1)).perform(click());
-
         onView(withId(brick_change_variable_label)).check(matches(isDisplayed()));
-        checkIfSpinnerOnBrickAtPositionShowsString(R.id.change_variable_spinner, 1, R.string
-                .testName2);
-        onView(withId(R.id.brick_change_variable_edit_text)).perform(click()).check(matches(isDisplayed()));
-        // onView(withId(R.id.formula_editor_keyboardview)).check(matches(isDisplayed()));
-
-        onView(withId(R.id.formula_editor_keyboard_data)).perform(click()).check(matches(isDisplayed()));
-
+//        onScriptList().atPosition(1).onChildView(withId(R.id.change_variable_spinner)).onChildView(withText(secondUserVariableName))
+//                .check(matches(isDisplayed()));
+        onScriptList().atPosition(1).onChildView(withId(R.id.brick_change_variable_edit_text)).perform(click());
+        onView(withId(R.id.formula_editor_keyboardview)).check(matches(isDisplayed()));
+        onView(withId(R.id.formula_editor_keyboard_data)).perform(click());
         onView(withText(secondUserVariableName)).perform(longClick());
-
-        onView(withId(R.string.delete)).perform(click()).check(matches(isDisplayed()));
-        //for warning dialog 
-        onView(withId(R.string.deletion_alert_yes)).perform(click()).check(matches(isDisplayed()));
+        onView(withText(R.string.delete)).perform(click());
+        //for warning dialog
+        onView(withText(R.string.deletion_alert_title))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.deletion_alert_yes)).perform(click());
         Espresso.pressBack();
         onView(withId(R.id.formula_editor_keyboardview)).check(matches(isDisplayed()));
-
-        checkIfSpinnerOnBrickAtPositionShowsString(R.id.change_variable_spinner, 1, R.string
-                .testName1);
         Espresso.pressBack();
-        onView(withId(brick_change_variable_label)).check(matches(isDisplayed()));
-
-
+        onScriptList().atPosition(1).onChildView(withId(R.id.change_variable_spinner)).onChildView(withText(userVariableName))
+                .check(matches(isDisplayed()));
     }
 
     @After
@@ -105,9 +94,5 @@ public class ChangeVariableBrickTest {
         Script startScript = BrickTestUtils.createProjectAndGetStartScript(UiTestUtils.DEFAULT_TEST_PROJECT_NAME);
         ChangeVariableBrick changeVariableBrick = new ChangeVariableBrick(10);
         startScript.addBrick(changeVariableBrick);
-
-
     }
-
 }
-s
